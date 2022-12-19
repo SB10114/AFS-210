7860ae6167b9b9c19708bcd24625a47a8a9287a7
Sierra <sierra.butler@bryanuniversity.edu>
2022-12-19 13:49:12 -0600

    save

---
+++ week2/datastructures.py
@@ -0,0 +1,23 @@
+data1 = (7, False, "Apple", True, 98.6)
+
+print(len(data1))
+print(data1[4])
+print(data1.count(7))
+
+
+data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
+
+print(data2.pop())
+data2.add("Alpha")
+print(data2)
+
+
+data3 = ["A", 7, -1, 3.14, True, 7]
+
+data3.reverse()
+data3.insert(1, "B")
+data3.pop()
+print(data3)
+
+
+data4 = {'name' = "Joe",  "age" = 26,   "active" = True,  "hourly_wage" = 14.75}
