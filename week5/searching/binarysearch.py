460a1ffec19c3bbef0cda4263fe4f791433b0ba3
Sierra <sierra.butler@bryanuniversity.edu>
2023-01-09 14:16:04 -0600

    commit 3

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -1,13 +1,34 @@
-def search(arr, target):
-    arr_size = len(arr)
-    for i in range(arr_size):
-        if target == arr[i]:
+# def search(arr, target): #time complexity is 0(n) this is a linear list
+#     arr_size = len(arr)
+#     for i in range(arr_size):
+#         if target == arr[i]:
+#             return True
+#         elif arr[i] > target:
+#             return False
+#     return False
+
+def binarySearch(arr, target):
+    start = 0
+    end = len(arr) -1
+    
+    while start <= end:
+        midpoint = (start + end) // 2
+        if arr[midpoint] == target:
             return True
-        elif arr[i] > target:
-            return False
-    return False
+        else:
+            if target < arr[midpoint]:
+                end = midpoint -1
+            else:
+                start = midpoint +1
+    
+        return False
 
 
 my_list = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
+my_list2 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
+my_list3 = ["Alpha", "Beta", "Delta","Epsilon", "Gamma", "Theta", "Zeta"]
 
-print(search(my_list, 40))
+
+print(binarySearch(my_list, 40))
+print(binarySearch(my_list2, 22))
+print(binarySearch(my_list3, 'Alpha'))
--- week5/sorting/mergesort.py
+++ week5/sorting/mergesort.py
@@ -3,39 +3,39 @@ def mergeSort(nlist):
     
     # insert your code to complete the mergeSort function
     if len(nlist) > 1:
-        leftArr = nlist[:len(nlist)//2]
-        rightArr = nlist[len(nlist)//2:]
-        merge(nlist, leftArr, rightArr) 
+        leftArr = nlist[:len(nlist)//2] #left side of mid   
+        rightArr = nlist[len(nlist)//2:] # right side of mid
+        merge(nlist, leftArr, rightArr) # left right merge to return nlist changed into full correct order 
 
-        mergeSort(leftArr)
-        mergeSort(rightArr)
-        merge(nlist, rightArr, leftArr)
+        mergeSort(leftArr) #recursively call nlist left side
+        mergeSort(rightArr)# recursively call nlist right side
+        merge(nlist, rightArr, leftArr) #merge right and then left into one list in proper order
        
     print("Merging ",nlist)
     
 
-def merge(nlist,lefthalf,righthalf):
-    i=j=k=0       
-    while i < len(lefthalf) and j < len(righthalf):
-        if lefthalf[i] < righthalf[j]:
-            nlist[k]=lefthalf[i]
-            i=i+1
+def merge(nlist,lefthalf,righthalf):# compares left to right
+    i=j=k=0       #counters I= left J= right K= final list
+    while i < len(lefthalf) and j < len(righthalf): #while there are values inside of the list in left and right
+        if lefthalf[i] < righthalf[j]: #compare positions
+            nlist[k]=lefthalf[i] #put it in list at the left position   
+            i=i+1 #increment by +1
         else:
-            nlist[k]=righthalf[j]
+            nlist[k]=righthalf[j] #put it in position at the right of k
             j=j+1
-        k=k+1
+        k=k+1 #increment K in all instancess
 
-    while i < len(lefthalf):
-        nlist[k]=lefthalf[i]
-        i=i+1
-        k=k+1
+    while i < len(lefthalf): #check ig I is less than the length (sorts left side)
+        nlist[k]=lefthalf[i] #add left half to final list
+        i=i+1 #increment
+        k=k+1 #increment K for end list
 
-    while j < len(righthalf):
-        nlist[k]=righthalf[j]
-        j=j+1
-        k=k+1
-    return nlist
+    while j < len(righthalf): #already sorted left half, merge it to the end on the right side of list (sorts right side only)
+        nlist[k]=righthalf[j] #add values on the right side of list to the end of list
+        j=j+1 #increment moving down the right side of the list
+        k=k+1 #moving position of nlist down to add more values
+    return nlist #returns final list
 
 nlist = [ 55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
-mergeSort(nlist)
-print("Sort: ",nlist)
+mergeSort(nlist) #call function
+print("Sort: ",nlist) #print final list
